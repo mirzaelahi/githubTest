59ac38fd126a15e791e3ea51ebfd11414682fdf0
Mirza Elahi <me5vp@virginia.edu>
2016-11-19 17:31:57 -0500

    Fix mySub for merging

--- mySub.cpp
+++ mySub.cpp
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int mySub(int a, int b){
-<<<<<<< HEAD
-    return (a-b);
-||||||| merged common ancestors
-    return a-b;
-=======
     int c = a-b;
     return c;
->>>>>>> merge-test
-
 }
